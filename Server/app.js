7b77f0c4d5a720eb76a493bb0af151d0e4bf8da9
Rahul Chauhan <rahulchauhan962103@gmail.com>
2025-08-10 01:28:47 +0530

    adding server code

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,28 @@
 import express from 'express';
+import dbConnect from './config/dbConnection.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+
+const startServer = async () => {
+    try {
+        // console.log("PORT:", process.env.PORT);
+        // console.log("MONGO_URI:", process.env.MONGO_URI);
+        
+        await dbConnect();
+        
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
